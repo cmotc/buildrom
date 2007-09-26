a260357516217933208f7d8bd62ca4b0740708fe
rminnich <rminnich@0fe3215d-741a-0410-bb5a-0d2cd39d948f>
2007-09-26 20:09:07 +0000

    alix1c support.
    trivial (but tested) -- bring msm800sev up to date
    
    Signed-off-by: Ronald G. Minnich <rminnich@gmail.com>
    Acked-by: Stefan Reinauer <stepan@coresystems.de>

---
+++ buildrom-devel/packages/kernel/alix1c-kernel.mk
@@ -0,0 +1,19 @@
+# Build file for the PCENGINES ALIX1.C LAB kernel
+
+KERNEL_URL=http://kernel.org/pub/linux/kernel/v2.6/
+KERNEL_SOURCE=linux-$(KERNEL_VERSION).tar.bz2
+KERNEL_CONFIG=$(PACKAGE_DIR)/kernel/conf/defconfig-alix1c
+
+#TINY_DIR=$(PACKAGE_DIR)/kernel/patches/tiny
+#KERNEL_PATCHES += $(shell ls $(TINY_DIR)/*.patch)
+
+
+$(SOURCE_DIR)/$(KERNEL_SOURCE):
+	@ mkdir -p $(SOURCE_DIR)
+	@ wget -P $(SOURCE_DIR) $(KERNEL_URL)/$(KERNEL_SOURCE)
+
+include $(PACKAGE_DIR)/kernel/kernel.inc
+
+kernel: generic-kernel
+kernel-clean: generic-kernel-clean
+kernel-distclean: generic-kernel-distclean
