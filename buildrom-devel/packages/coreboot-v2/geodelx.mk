cc5d5e6e703326b73914d0dde60e75079e5f961a
jcrouse <jcrouse@0fe3215d-741a-0410-bb5a-0d2cd39d948f>
2008-02-22 21:50:12 +0000

    [BUILDROM] Fixup VSA fetching code
    In preparation for v3 and the upcoming openvsa changes,
    rearrange the VSA fetching code.  We now fetch an 
    uncompressed blob, and do the compressing within buildrom.
    I'm mainly concentrating on v2 right now (this is not quite
    v3 friendly), but it will be.
    
    Also, work around a hopefully temporary bug on the AMD
    servers.
    
    Signed-off-by: Jordan Crouse <jordan.crouse@amd.com>
    Acked-by: Ward Vandewege <ward@gnu.org>

--- buildrom-devel/packages/coreboot-v2/geodelx.mk
+++ buildrom-devel/packages/coreboot-v2/geodelx.mk
@@ -10,15 +10,10 @@ CBV2_BASE_DIR=svn
 CBV2_URL=svn://coreboot.org/repos/trunk/coreboot-v2
 CBV2_TARBALL=coreboot-svn-$(CBV2_TAG).tar.gz
 CBV2_PAYLOAD_TARGET=$(CBV2_BUILD_DIR)/payload.$(CBV2_PAYLOAD_FILE_EXT)
-VSA_URL=http://www.amd.com/files/connectivitysolutions/geode/geode_lx/
-CBV2_VSA=lx_vsa.36k.bin
+
 TARGET_ROM = $(COREBOOT_VENDOR)-$(COREBOOT_BOARD).rom
 
 include $(PACKAGE_DIR)/coreboot-v2/coreboot.inc
-
-$(SOURCE_DIR)/$(CBV2_VSA):
-	@ echo "Fetching the VSA blob..."
-	wget -P $(SOURCE_DIR) $(VSA_URL)/$(CBV2_VSA).gz  -O $@
 
 $(SOURCE_DIR)/$(CBV2_TARBALL):
 	@ echo "Fetching the coreboot rev $(CBV2_TAG) code..."
@@ -27,12 +22,10 @@ $(SOURCE_DIR)/$(CBV2_TARBALL):
 	$(CBV2_TAG) $(SOURCE_DIR)/$(CBV2_TARBALL) \
 	> $(CBV2_FETCH_LOG) 2>&1
 
-# Special rule - append the VSA
-
-$(OUTPUT_DIR)/$(TARGET_ROM): $(CBV2_OUTPUT) $(SOURCE_DIR)/$(CBV2_VSA)
+$(OUTPUT_DIR)/$(TARGET_ROM): $(CBV2_OUTPUT) $(GEODE_PADDED_VSA)
 	@ mkdir -p $(OUTPUT_DIR)
-	@ cat $(SOURCE_DIR)/$(CBV2_VSA) $(CBV2_OUTPUT) > $@
+	@ cat $(GEODE_PADDED_VSA) $(CBV2_OUTPUT) > $@
 
-coreboot: $(OUTPUT_DIR)/$(TARGET_ROM)
-coreboot-clean: generic-coreboot-clean
-coreboot-distclean: generic-coreboot-distclean
+coreboot: geodevsa $(OUTPUT_DIR)/$(TARGET_ROM)
+coreboot-clean: geodevsa-clean generic-coreboot-clean
+coreboot-distclean: geodevsa-distclean generic-coreboot-distclean
