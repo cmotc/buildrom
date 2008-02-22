f1be834878e09cea620e8be4fd80b214a76e262f
jcrouse <jcrouse@0fe3215d-741a-0410-bb5a-0d2cd39d948f>
2008-02-22 21:52:09 +0000

    Grr - seriously.  Who manages directories? Part of the previous patch

---
+++ buildrom-devel/packages/geodevsa/geodevsa.mk
@@ -0,0 +1,33 @@
+# Master targets for VSA manipulation
+
+GEODE_UNCOMPRESSED_VSA=$(OUTPUT_DIR)/vsa/geodevsa.bin
+GEODE_COMPRESSED_VSA=$(OUTPUT_DIR)/vsa/geodevsa.bin.nrv
+GEODE_PADDED_VSA=$(OUTPUT_DIR)/vsa/geodevsa.bin.nrv.pad
+
+GEODE_VSA_SIZE=36864
+
+ifeq ($(CONFIG_COREBOOT_V2),y)
+VSA_BUILD_TARGET = $(GEODE_PADDED_VSA)
+else
+VSA_BUILD_TARGET = $(GEODE_UNCOMPRESSED_VSA)
+endif
+
+include $(PACKAGE_DIR)/geodevsa/amdvsa.inc
+
+$(GEODE_COMPRESSED_VSA): $(GEODE_UNCOMPRESSED_VSA)
+	@ $(STAGING_DIR)/bin/nrv2b e $(GEODE_UNCOMPRESSED_VSA) $@ \
+	> /dev/null 2>&1
+
+$(GEODE_PADDED_VSA): $(GEODE_COMPRESSED_VSA)
+	@ cp $< $@
+	@ (size=`stat -c %s $<`; count=`expr $(GEODE_VSA_SIZE) - $$size`; \
+	dd if=/dev/zero bs=1 count=$$count  >> $@ 2> /dev/null)
+
+geodevsa: $(VSA_BUILD_TARGET)
+
+geodevsa-clean:
+	@ rm -f $(GEODE_UNCOMPRESSED_VSA) $(GEODE_COMPRESSED_VSA)
+	@ rm -f $(GEODE_PADDED_VSA)
+
+geodevsa-distclean:
+	@ rm -rf $(OUTPUT_DIR)/vsa
