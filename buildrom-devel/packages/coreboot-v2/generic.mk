9b9aade9b02e022e63485d0f5e72a921e7d620ce
myles <myles@0fe3215d-741a-0410-bb5a-0d2cd39d948f>
2008-05-06 15:30:32 +0000

    This makes it so that checking the "USE_LZMA" option in buildrom doesn't break builds.  It moves the USE_LZMA option to the payload menu, since it only affects the payload.
    It assumes that all Config.lb files don't use compression, and all
    Config-lab.lb files do.  It also updates qemu.conf to use 3281 which has the
    correct Config-lab.lb.
     
    Signed-off-by: Myles Watson <mylesgw@gmail.com>
    Acked-by: Jordan Crouse <jordan.crouse@amd.com>

--- buildrom-devel/packages/coreboot-v2/generic.mk
+++ buildrom-devel/packages/coreboot-v2/generic.mk
@@ -1,20 +1,5 @@
 # This is the Generic coreboot target
 
-ifeq ($(CONFIG_PAYLOAD_OFW),y)
-	CBV2_CONFIG=Config-lab.lb
-	CBV2_PAYLOAD_FILE_EXT=elf.lzma
-endif
-
-ifeq ($(CONFIG_PAYLOAD_KERNEL),y)
-	CBV2_CONFIG=Config-lab.lb
-	CBV2_PAYLOAD_FILE_EXT=elf.lzma
-endif
-
-ifeq ($(CONFIG_PAYLOAD_LAB),y)
-	CBV2_CONFIG=Config-lab.lb
-	CBV2_PAYLOAD_FILE_EXT=elf.lzma
-endif
-
 include $(PACKAGE_DIR)/coreboot-v2/coreboot.inc
 
 coreboot: generic-coreboot
