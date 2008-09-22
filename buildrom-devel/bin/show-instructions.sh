704f23fefa570559faab7e26064eb19beaf4df9f
jcrouse <jcrouse@0fe3215d-741a-0410-bb5a-0d2cd39d948f>
2008-09-22 21:30:20 +0000

    If lxdialog doesn't exist for some reason, then output the instructions to stdout.  Trivial.
    Signed-off-by: Jordan Crouse <jordan.crouse@amd.com>
    Acked-by: Jordan Crouse <jordan.crouse@amd.com>

--- buildrom-devel/bin/show-instructions.sh
+++ buildrom-devel/bin/show-instructions.sh
@@ -2,6 +2,11 @@
 
 tmp=`tempfile`
 cat $1 | sed -e "s:%DESTFILE%:$2:" > $tmp
+
+if [ -x `dirname $0`/../scripts/kconfig/lxdialog/lxdialog ]; then
 `dirname $0`/../scripts/kconfig/lxdialog/lxdialog --textbox $tmp 20 75
+else
+cat $tmp
+fi
 rm -rf $tmp
 
