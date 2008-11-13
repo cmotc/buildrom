b653404d7c7c454f8592b0a4b48d772a9f5507d2
mjones <mjones@0fe3215d-741a-0410-bb5a-0d2cd39d948f>
2008-11-13 18:55:51 +0000

    Skip the VBIOS check if building a headless version.
    Signed-off-by: Marc Jones <marcj303@yahoo.com>
    Acked-by: Jordan Crouse <jordan@cosmicpenguin.net>

--- buildrom-devel/packages/roms/amd_r690.mk
+++ buildrom-devel/packages/roms/amd_r690.mk
@@ -2,7 +2,6 @@ ifeq ($(CONFIG_AMD_R690_USE_VBIOS),y)
 R690_TARGET_ROM := $(shell basename $(AMD_R690_VBIOS_LOCATION))
 OPTIONROM_TARGETS += $(OUTPUT_DIR)/roms/$(R690_TARGET_ROM)
 CBV2_PREPEND := $(OUTPUT_DIR)/roms/$(R690_TARGET_ROM)
-endif
 
 $(AMD_R690_VBIOS_LOCATION):
 	@ $(BIN_DIR)/show-instructions.sh \
@@ -14,3 +13,4 @@ $(AMD_R690_VBIOS_LOCATION):
 $(OUTPUT_DIR)/roms/$(R690_TARGET_ROM): $(AMD_R690_VBIOS_LOCATION)
 	@ mkdir -p $(OUTPUT_DIR)/roms
 	@ cp $< $@
+endif
