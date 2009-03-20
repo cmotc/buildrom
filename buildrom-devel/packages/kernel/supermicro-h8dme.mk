55e220bf866a33f0912a22711b416ec3c8592b2c
ward <ward@0fe3215d-741a-0410-bb5a-0d2cd39d948f>
2009-03-20 17:07:35 +0000

    Add Supermicro H8DME to buildrom.
    Signed-off-by: Ward Vandewege <ward@gnu.org>
    Acked-by: Peter Stuge <peter@stuge.se>
    Acked-by: Marc Jones <marcj303@gmail.com>

---
+++ buildrom-devel/packages/kernel/supermicro-h8dme.mk
@@ -0,0 +1,26 @@
+# Build file for the Supermicro h8dme LAB kernel
+
+KERNEL_URL=http://kernel.org/pub/linux/kernel/v2.6/
+KERNEL_SOURCE=linux-$(KERNEL_VERSION).tar.bz2
+
+# KERNEL_CONFIG is defined in the platform configuration
+
+TINY_URL=http://elinux.org/images/0/0e/
+TINY_SOURCE=Tiny-quilt-2.6.22.1-1.tar.gz
+TINY_DIR=$(KERNEL_DIR)/tiny/patches
+
+KERNEL_PATCHES += $(TINY_DIR)
+
+$(SOURCE_DIR)/$(KERNEL_SOURCE):
+	@ mkdir -p $(SOURCE_DIR)
+	@ wget $(WGET_Q) -P $(SOURCE_DIR) $(KERNEL_URL)/$(KERNEL_SOURCE)
+
+$(SOURCE_DIR)/$(TINY_SOURCE):
+	@ mkdir -p $(SOURCE_DIR)
+	@ wget $(WGET_Q) -P $(SOURCE_DIR) $(TINY_URL)/$(TINY_SOURCE)
+
+include $(PACKAGE_DIR)/kernel/kernel.inc
+
+kernel: generic-kernel
+kernel-clean: generic-kernel-clean
+kernel-distclean: generic-kernel-distclean
